7a49ab7701d30c285151b77d6888625de7eb267d
Bryan Tai <redhatpieman@gmail.com>
2020-01-01 11:43:34 -0800

    Initial Commit!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gets the input, forms a vector 3 value and moves the player using the attached rigidbody
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+    }
+}
