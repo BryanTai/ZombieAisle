4065e6b9a70a8540145c470b420f31fdb9e97f11
Bryan Tai <redhatpieman@gmail.com>
2020-01-16 22:10:13 -0800

    Created simple zombie prefab and script

---
+++ Assets/Scripts/GruntController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GruntController : MonoBehaviour
+{
+    public int hitPoints;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    //TODO: hit by raycast
+    public void OnShot()
+    {
+
+    }
+}
