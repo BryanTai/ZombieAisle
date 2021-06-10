8600e07b878360514443c795028c7765b7a177c0
Bryan Tai <redhatpieman@gmail.com>
2021-06-10 09:55:04 -0700

    Added logic to Plan Screen hour input buttons

---
+++ Assets/Scripts/PlanRow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanRow : MonoBehaviour
+{
+	public delegate void OnHoursChanged();
+
+	[Header("Cosmetic Elements")]
+	[SerializeField] private Text _rowTitle;
+
+	[Header("Input Elements")]
+	[SerializeField] private Text _hoursText;
+	[SerializeField] private Button _plusButton;
+	[SerializeField] private Button _minusButton;
+
+	public int Hours { get { return _hours; } }
+
+	private int _hours;
+	private OnHoursChanged _onHoursChangedCallback;
+
+	public void Init(int initHours, OnHoursChanged callback)
+	{
+		_onHoursChangedCallback = callback;
+		_hours = initHours;
+		OnHoursButtonPressed();
+	}
+
+	public void TogglePlusButton(bool enabled)
+	{
+		_plusButton.interactable = enabled;
+	}
+
+	#region Button Callbacks
+	public void OnPlusButtonPressed()
+	{
+		_hours++;
+		OnHoursButtonPressed();
+	}
+
+	public void OnMinusButtonPressed()
+	{
+		_hours--;
+		OnHoursButtonPressed();
+	}
+
+	private void OnHoursButtonPressed()
+	{
+		_minusButton.interactable = (_hours > 0);
+		_hours = Mathf.Max(0, _hours);
+		_hoursText.text = _hours.ToString();
+		_onHoursChangedCallback.Invoke();
+	}
+	#endregion
+}
--- Assets/Scripts/ScreenControllers/PlanScreenController.cs
+++ Assets/Scripts/ScreenControllers/PlanScreenController.cs
@@ -5,12 +5,31 @@ using UnityEngine.UI;
 
 public class PlanScreenController : RestStateController
 {
+	private const int TOTAL_HOURS_IN_DAY = 12;
 	[SerializeField] private Button _startNightButton;
+
+	[Header("Hour Text fields")]
+	[SerializeField] private Text _hoursAvailableText;
+	[SerializeField] private PlanRow[] _planRows = new PlanRow[3];
+
+	private int _totalHoursRemaining;
 
 	protected override void InitReferences()
 	{
 		base.InitReferences();
 		_startNightButton.onClick.AddListener(OnStartButtonClicked);
+
+		InitPlanRows();
+	}
+
+	private void InitPlanRows()
+	{
+		foreach(PlanRow planRow in _planRows)
+		{
+			planRow.Init(0, OnHoursChanged);
+		}
+
+		OnHoursChanged();
 	}
 
 	private void OnStartButtonClicked()
@@ -24,4 +43,28 @@ public class PlanScreenController : RestStateController
 		//TODO: implement
 		Debug.LogWarning("[PlanScreenController] Not implemented yet!");
 	}
+
+	#region Input Functions
+
+	public void OnHoursChanged()
+	{
+		int totalHoursAssigned = 0;
+		foreach(PlanRow planRow in _planRows)
+		{
+			totalHoursAssigned += planRow.Hours;
+		}
+
+		_totalHoursRemaining = TOTAL_HOURS_IN_DAY - totalHoursAssigned;
+		_totalHoursRemaining = Mathf.Max(0, _totalHoursRemaining);
+		_hoursAvailableText.text = _totalHoursRemaining.ToString();
+
+		bool canAssignHours = _totalHoursRemaining > 0;
+
+		foreach(PlanRow planRow in _planRows)
+		{
+			planRow.TogglePlusButton(canAssignHours);
+		}
+	}
+
+	#endregion
 }
